dd60f7549365b0f8a181112e9d26bced85da10b3
Mandorin <pitsanubomb@gmail.com>
2017-06-05 05:10:04 +0700

    ng-annotate

--- webpack.config.js
+++ webpack.config.js
@@ -86,7 +86,11 @@ module.exports = function makeWebpackConfig() {
       // Transpile .js files using babel-loader
       // Compiles ES6 and ES7 into ES5 code
       test: /\.js$/,
-      loader: 'babel-loader',
+      loader : [
+          {loader: 'babel-loader'},
+          { loader: 'ng-annotate-loader' }
+          
+      ],
       exclude: /node_modules/
     }, {
       // CSS LOADER
