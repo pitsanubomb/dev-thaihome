7ce33c1ef5ad474c4ff18524870b99482b57fad5
Mandorin <pitsanubomb@gmail.com>
2017-06-06 06:00:02 +0700

    remove foot and some css

--- app/main/app.js
+++ app/main/app.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import '../style/style.css';
 import appModule from './app.config';
 
